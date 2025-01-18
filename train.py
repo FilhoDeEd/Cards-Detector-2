91779e434337b3a0ec5ad1d5d707ee9de16a834a
FilhoDeEd <edson.er.rodrigues@gmail.com>
2025-01-17 23:43:21 -0300

    train script and requirements

---
+++ train.py
@@ -0,0 +1,26 @@
+import os
+from ultralytics import YOLO
+
+
+MODEL_NAME = 'cards_detector'
+YOLO_BASE_MODEL = 'yolov8n-obb.pt'
+DATASET_YAML_PATH = 'datasets/cards_detector_42/data.yml'
+IMAGE_SIZE = 960
+BATCH = 8
+EPOCHS = 50
+
+
+def main() -> None:
+    model = YOLO(YOLO_BASE_MODEL)
+    model.train(
+        data=os.path.abspath(DATASET_YAML_PATH),
+        epochs=EPOCHS,
+        batch=BATCH,
+        imgsz=IMAGE_SIZE,
+        project='runs/train',
+        name=MODEL_NAME
+    )
+
+
+if __name__ == '__main__':
+    main()
